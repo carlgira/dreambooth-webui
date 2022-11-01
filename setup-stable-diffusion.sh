62066a18bd4258f99024d16585d799617cb9914f
carlgira@gmail.com <carlgira@gmail.com>
2022-11-01 13:28:02 +0100

    fix echo in script

--- setup-stable-diffusion.sh
+++ setup-stable-diffusion.sh
@@ -5,7 +5,7 @@ git init
 git lfs install --system --skip-repo
 git remote add -f origin "https://USER:$HF_TOKEN@huggingface.co/runwayml/stable-diffusion-v1-5"
 git config core.sparsecheckout true
-echo -e "feature_extractor\nsafety_checker\nscheduler\ntext_encoder\ntokenizer\nunet\nmodel_index.json" > .git/info/sparse-checkout
+echo "feature_extractor\nsafety_checker\nscheduler\ntext_encoder\ntokenizer\nunet\nmodel_index.json" > .git/info/sparse-checkout
 git pull origin main
 
 git clone "https://USER:$HF_TOKEN@huggingface.co/stabilityai/sd-vae-ft-mse"
