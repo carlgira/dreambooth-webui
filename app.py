b7a8d983f4c2e671985e2eb2b35be19ae31872c4
carlgira@gmail.com <carlgira@gmail.com>
2022-11-02 12:50:37 +0100

    Refactoring, and adding error handling

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import subprocess
 from train import train_model
 from werkzeug.utils import secure_filename
 import threading
-import time
 
 
 CHECK_POINT_PATH_SD = '/home/ubuntu/stable-diffusion-webui/model.ckpt'
@@ -28,6 +27,7 @@ def home():
     global t
 
     # check if directory exists
+    '''
     if not os.path.exists(CHECK_POINT_PATH_SD):
         
         if request.method == 'GET':
@@ -37,27 +37,39 @@ def home():
         password = request.form['password']
         token = request.form['token']
         
-        # download the model from the server and use basic authentication with the username and password
-        req = requests.get(SD_URL, auth=(username, password))
-        # save the model in the current directory
-        with open(CHECK_POINT_PATH_SD, 'wb') as f:
-            f.write(req.content)
+        try:
+            # download the model from the server and use basic authentication with the username and password
+            req = requests.get(SD_URL, auth=(username, password))
+            # save the model in the current directory
+            with open(CHECK_POINT_PATH_SD, 'wb') as f:
+                f.write(req.content)
+        except:
+            return render_template('setup.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='Your credentials are incorrect. Please try again.')
         
         subprocess.run(["sudo", "systemctl", "start", "stabble-diffusion.service"])
         
-        sd_model = subprocess.run(["sh", "/home/ubuntu/dreambooth-webui/setup-stable-diffusion.sh", token])
         
-        return render_template('index.html')
+        try:
+            sd_model = subprocess.run(["sh", "/home/ubuntu/dreambooth-webui/setup-stable-diffusion.sh", token], check=True)            
+        except:
+            return render_template('setup.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='The token is incorrect. Please try again.')
+        
+        
+        return render_template('messages.html', MESSAGE_TITLE='Information', MESSAGE_CONTENT='The model has been downloaded successfully. Now you have to wait 5 minutes for the web app to be ready.')
     
     if not os.path.exists(SD_RAW_MODEL):
         if request.method == 'GET':
             return render_template('setup.html')
         
         token = request.form['token']
-        sd_model = subprocess.run(["sh", "/home/ubuntu/dreambooth-webui/setup-stable-diffusion.sh", token])
-        print(sd_model)
-        return render_template('index.html')
-
+        
+        try:
+            sd_model = subprocess.run(["sh", "/home/ubuntu/dreambooth-webui/setup-stable-diffusion.sh", token], check=True)            
+        except:
+            return render_template('setup.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='The token is incorrect. Please try again.')
+        
+        return render_template('messages.html', MESSAGE_TITLE='Information', MESSAGE_CONTENT='The model has been downloaded successfully. Now you have to wait 5 minutes for the web app to be ready.')
+    '''
     if request.method == 'POST':
         
         if t is None or not t.is_alive():
@@ -76,24 +88,31 @@ def home():
                 return render_template('index.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='No zip file uploaded')
             
             filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            try:
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+            except:
+                return render_template('index.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='The zip file could not be saved')
             
             # unzip the file
             subprocess.run(["rm", "-rf", UPLOAD_FOLDER + '/' + instance_name])
-            unzip = subprocess.run(["unzip", UPLOAD_FOLDER + '/' + filename, '-d' , UPLOAD_FOLDER + '/' + instance_name])
+            try:
+                unzip = subprocess.run(["unzip", UPLOAD_FOLDER + '/' + filename, '-d' , UPLOAD_FOLDER + '/' + instance_name], check=True)
+            except:
+                return render_template('index.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='There was an error while unzipping the file, please check the file and try again.')
+            
             subprocess.run(["rm", "-rf", UPLOAD_FOLDER + '/' + filename])
 
             # call train_model function in a new thread
             t = threading.Thread(target=train_model, args=(training_subject, subject_type, instance_name, class_dir, training_steps, seed))
             t.start()
             
-            return render_template('index.html', MESSAGE_TITLE='Training', MESSAGE_CONTENT='Training in progress', IS_RUNNING=True)
+            return redirect('/')
         else:
-            return render_template('index.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='Training already in progress. You can reload this page to know if the process has ended.', IS_RUNNING=True)
+            return render_template('index.html', MESSAGE_TITLE='Error', MESSAGE_CONTENT='Training already in progress, it should take an hour. You can reload this page to know if the process has ended.', IS_RUNNING=True)
     
     if request.method == 'GET':
         if t is not None and t.is_alive():
-            return render_template('index.html', MESSAGE_TITLE='Information', MESSAGE_CONTENT='Training already in progress', IS_RUNNING=True)
+            return render_template('index.html', MESSAGE_TITLE='Information', MESSAGE_CONTENT='Training already in progress, it should take an hour. You can reload this page to know if the process has ended.', IS_RUNNING=True)
     
     return render_template('index.html')
 
