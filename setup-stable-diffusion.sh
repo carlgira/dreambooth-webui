565593b86e6f98b5135bd03103167444d3f69f25
carlgira@gmail.com <carlgira@gmail.com>
2022-12-04 21:36:36 +0100

    Change sd model 512

--- setup-stable-diffusion.sh
+++ setup-stable-diffusion.sh
@@ -5,7 +5,7 @@ set -e
 cd $WORK_DIR/stable-diffusion
 git init
 git lfs install --system --skip-repo
-git remote add -f origin "https://USER:$HF_TOKEN@huggingface.co/stabilityai/stable-diffusion-2"
+git remote add -f origin "https://USER:$HF_TOKEN@huggingface.co/stabilityai/stable-diffusion-2-base"
 git config core.sparsecheckout true
 printf "scheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json" > .git/info/sparse-checkout
 git pull origin main
