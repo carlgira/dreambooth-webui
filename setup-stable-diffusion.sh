d4f231378dfea8b583a4dae244c0a7ebc17a6aa3
carlgira@gmail.com <carlgira@gmail.com>
2022-12-04 12:02:19 +0100

    Fixing git clone sd

--- setup-stable-diffusion.sh
+++ setup-stable-diffusion.sh
@@ -7,7 +7,7 @@ git init
 git lfs install --system --skip-repo
 git remote add -f origin "https://USER:$HF_TOKEN@huggingface.co/stabilityai/stable-diffusion-2"
 git config core.sparsecheckout true
-printf "text_encoder\ntokenizer\nunet\nvae\nmodel_index.json" > .git/info/sparse-checkout
+printf "scheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json" > .git/info/sparse-checkout
 git pull origin main
 
 rm -rf .git
