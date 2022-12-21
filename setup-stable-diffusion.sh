be4842699d8f9e0caed0920a5b422720ceed63ce
carlgira@gmail.com <carlgira@gmail.com>
2022-12-21 12:42:38 +0100

    Adding missing file sd

--- setup-stable-diffusion.sh
+++ setup-stable-diffusion.sh
@@ -7,7 +7,7 @@ git init
 git lfs install --system --skip-repo
 git remote add -f origin "https://USER:$HF_TOKEN@huggingface.co/stabilityai/stable-diffusion-2-1-base"
 git config core.sparsecheckout true
-printf "scheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json" > .git/info/sparse-checkout
+printf "feature_extractor\nscheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json" > .git/info/sparse-checkout
 git pull origin main
 
 rm -rf .git
